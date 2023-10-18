4779252703efa05c7d63be3792222fdcf83adc5a
The_Typhoon1 <115441680+TheTyphoon1@users.noreply.github.com>
2023-10-18 11:52:17 +1100

    Add files via upload

---
+++ annoyance.bat
@@ -0,0 +1,27 @@
+@echo off
+color 04
+echo You are now breathing manually
+timeout /t 1 > nul
+echo.
+echo You are now blinking manually
+timeout /t 1 > nul
+echo.
+echo You are now aware your jaw has weight
+timeout /t 1 > nul
+echo.
+echo You are now aware your head has weight
+timeout /t 1 > nul
+echo.
+echo You are now aware your limbs have weight
+timeout /t 1 > nul
+echo. 
+timeout /t 1 > nul
+echo.
+echo  Latitude: -37.837853 / S 37° 50' 16.271'' Longitude: 145.233143 / E 145° 13' 59.313'' Heathmont College, 8-50 Waters Grove, Heathmont VIC 3135, Australia, Oceania, Southern Ocean.
+timeout /t 1 > nul
+echo.
+echo Connection-specific DNS Suffix  . : curric.heathmont-co.wan, Link-local IPv6 Address: fe80::ac1:cf3a:efdf:d0c0%13, IPv4 Address: 10.131.225.153
+timeout /t 1 > nul
+echo.
+echo Subnet Mask: 255.255.248.0, Default Gateway: 10.131.224.1
+timeout /t 9999 > nul
